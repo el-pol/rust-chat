26eaca68f2a96efe1c061198258f040062d442b3
Pol Milian <pol.milian.dev@gmail.com>
2023-02-07 19:18:43 +0800

    start a server that echoes what we write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let mut buffer: [u8; 1024] = [0u8; 1024];
+    let bytes_read: usize = socket.read(&mut buffer).await.unwrap();
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();
 }
